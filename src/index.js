053faea8c6a47da0e41dd145304fe5eed39fcd7f
Julia <juliariety@gmail.com>
2022-05-31 21:45:35 +0200

    Port to Cloudflare Workers
    This is very hacked together, but it seems to work well

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
 
-export default {
-  async fetch(request) {
-    return new Response("Hello World!");
-  },
-};
+async function handleRequest(request) {
+  let image = await KV.get("image", {
+    type: "arrayBuffer",
+  });
+
+  if (image) {
+    return new Response(image, {
+      headers: { "Content-Type": "image/jpeg" },
+    });
+  }
+
+  const BING_HOSTNAME = "https://www.bing.com";
+  const IMAGE_ARCHIVE_URL = `${BING_HOSTNAME}/HPImageArchive.aspx?format=js&idx=0&n=1`;
+
+  const imageArchive = await fetch(IMAGE_ARCHIVE_URL);
+  const imageArchiveJson = await imageArchive.json();
+  const imageUrl = `${BING_HOSTNAME}/${imageArchiveJson.images[0].url}`;
+  image = await fetch(imageUrl);
+
+  const buf = await image.arrayBuffer();
+
+  await KV.put("image", buf, {
+    expirationTtl: 60 * 60 * 24,
+  });
+
+  return new Response(buf, {
+    headers: { "Content-Type": "image/jpeg" },
+  });
+}
