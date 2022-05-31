523be702156ad3ef24afd051e76b9b0d8bf2d300
Julia <juliariety@gmail.com>
2022-05-31 14:21:33 +0200

    Implement Bing background image service

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from typing import Optional
+import os
+from urllib.parse import urljoin as join_url
 
+import httpx
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+MARKET = os.environ.get("MARKET", "en-US")
+BING_HOSTNAME = "https://www.bing.com"
+IMAGE_ARCHIVE_URL = join_url(
+    BING_HOSTNAME, f"/HPImageArchive.aspx?format=js&idx=0&n=1&mkt={MARKET}"
+)
+
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root(name: Optional[str] = "World"):
-    return {"message": f"Hi, {name}!"}
+async def root(response_class=RedirectResponse):
+    with httpx.AsyncClient() as client:
+        response = await client.get(IMAGE_ARCHIVE_URL)
+        data = response.json()
+        image_url = data["images"][0]["url"]
+
+        return join_url(BING_HOSTNAME, image_url)
